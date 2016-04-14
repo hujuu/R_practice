4e82c77383e8b8e5801cc0799253cdce02171289
ken <out.of.the.base.sfc@gmail.com>
2016-04-14 16:18:06 +0900

    練習続行ちゅう

--- r_mining_1.R
+++ r_mining_1.R
@@ -13,3 +13,39 @@ x <- c(3,6,10)
 x
 y <- c(3,2,5)
 x/y
+
+x^2
+
+#2次元配列（行列）
+x <- 1:9
+dim(x) <- c(3,3)
+
+#3次元配列
+x <- 1:27
+dim(x) <- c(3,3,3)
+x
+
+matrix(1:15, ncol = 5, byrow = TRUE)
+cbind(1:2,4:5,7:8)
+
+ws.customer <- read.csv("Wholesale customers data.csv")
+head(ws.customer)
+cor(ws.customer[, 3:6])
+
+install.packages("dplyr")
+library(dplyr)
+filter(ws.customer, Frozen>8000, Grocery>9000)
+filter(ws.customer, Frozen>8000 | Grocery>9000)
+select(ws.customer, Channel:Milk)
+
+library(ggplot2)
+
+channel.tab <- table(ws.customer$Channel)
+channel.tab
+barplot(channel.tab, ylim = c(0,300), ylab = "度数")
+
+###plotで文字化け防止#####
+#初期設定では文字化け
+?par
+par(family = "")
+par(family = "HiraKakuProN-W3")
