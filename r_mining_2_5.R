8e3bb02f0dcd065db382802215c520cfea2ac1a6
ken <out.of.the.base.sfc@gmail.com>
2016-04-25 18:27:49 +0900

    回帰木分析

--- r_mining_2_5.R
+++ r_mining_2_5.R
@@ -1,2 +1,30 @@
 data(Titanic)
 str(Titanic)
+Titanic[,1,2,]
+
+mosaicplot(Titanic[,1,2,], color = T)
+
+install.packages("epitools")
+library(epitools)
+Titanic.df <- expand.table(Titanic)
+library(rpart)
+Titanic.tree <- rpart(Survived ~.,data = Titanic.df,method = "class")
+summary(Titanic.tree)
+
+install.packages("partykit")
+library(partykit)
+plot(as.party(Titanic.tree))
+
+plotcp(Titanic.tree)
+
+Titanic.tree2 <- rpart(Survived ~ ., data = Titanic.df,
+                       method = "class", cp = 0.083)
+summary(Titanic.tree2)
+plot(as.party(Titanic.tree2))
+
+library(ggplot2)
+data(diamonds)
+head(diamonds)
+diamonds2 <- subset(diamonds, subset = carat >= 1.5 & carat < 2 &
+                      clarity %in% c("I1","SI2"))
+boxplot(diamonds2$price)
