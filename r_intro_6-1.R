aa0e28e5748079ca46383da47aae1096b6a08cd6
Ken Nakai <owner@KenNakai.local>
2016-05-01 00:33:01 +0900

    6章の追加

--- r_intro_6-1.R
+++ r_intro_6-1.R
@@ -1,2 +1,14 @@
+#分散分析
+#データの読み込み
 sleep1 <- read.csv("R_practice/anoval.csv")
+
+#平均値の算出
 tapply(sleep1$time, sleep1$club, mean)
+
+#標準偏差
+tapply(sleep1$time, sleep1$club, sd)
+
+aov(sleep1$time~sleep1$club)
+
+summary(aov(sleep1$time~sleep1$club))
+TukeyHSD(aov(sleep1$time~sleep1$club))
