30493a079ee489767af7cb2de0b6c2c9cbd85a6b
ken.nakai <ken.nakai@gree.net>
2016-05-03 02:47:00 +0900

    分散分析の続き

--- r_intro_6-4.R
+++ r_intro_6-4.R
@@ -2,3 +2,22 @@ install.packages("reshape")
 library(reshape)
 
 task2 <- read.csv("R_practice/anova2b.csv")
+
+task2$number2 <- factor(task2$number)
+head(task2)
+
+task3 <- task2[,c("number2","music","fragrance","no")]
+head(task3)
+
+task4 <- melt(task3,id="number2")
+names(task4) <- c("number2", "condition", "grade")
+head(task4)
+
+summary(aov(task4$grade~task4$condition + task4$number2))
+
+TukeyHSD(aov(task4$grade~task4$condition + task4$number2))
+
+names(task4) <- c("number2", "variable", "value")
+task4
+task5 <- cast(task4)
+task5
---
+++ r_intro_6-5.R
@@ -0,0 +1,3 @@
+#データの読み込み
+sleep2 <- read.csv("R_practice/anova3a.csv")
+head(sleep2)
