18c018a4e972dcdc0a35db1d692d4ac715a031d2
hujuu <out.of.the.base.sfc@gmail.com>
2016-05-12 00:55:56 +0900

    6-7完成

--- r_intro_6-7.R
+++ r_intro_6-7.R
@@ -9,3 +9,32 @@ beer3
 
 beer4 <- melt(beer3, id="number2")
 beer4
+names(beer4) <- c("number2","condition","taste")
+head(beer4)
+beer4$place <- ifelse((beer4$condition=="beerAin"
+                       |beer4$condition=="beerBin"
+                       |beer4$condition=="beerCin"),"in","out")
+head(beer4)
+
+qbeer4$drink <- ifelse((beer4$condition=="beerAin"
+                       | beer4$condition=="beerAout"),"beerA",
+                      ifelse((beer4$condition=="beerBin"
+                              |beer4$condition=="beerBout"),"beerB","beerC"))
+class(beer4$drink)
+class(beer4$place)
+
+beer4$drink2 <- factor(beer4$drink)
+beer4$drink2
+
+beer4$place2 <- factor(beer4$place)
+beer4$place2
+
+beer5 <- beer4[,c("number2","drink2","place2","taste")]
+head(beer5)
+
+summary(aov(beer5$taste
+            ~beer5$drink2*beer5$place2
+            +Error(beer5$number2
+                   +beer5$number2:beer5$drink2
+                   +beer5$number2:beer5$place2
+                   +beer5$number2:beer5$drink2,beer5$place2)))
