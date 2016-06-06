09020bc4d80e2ac631861bc24ade23217815e248
hujuu <out.of.the.base.sfc@gmail.com>
2016-06-07 01:18:06 +0900

    6-10

---
+++ r-intro_6-10.R
@@ -0,0 +1,11 @@
+#アンバランスデザインの分散分析
+
+sleep3 <- read.csv("R_practice/anova3b.csv") # データの読み込み
+
+tapply(sleep3$time, list(sleep3$club, sleep3$sex), mean)
+
+tapply(sleep3$time, list(sleep3$club, sleep3$sex), sd)
+
+summary(aov(sleep3$time~sleep3$club*sleep3$sex))
+
+summary(aov(sleep3$time~sleep3$sex*sleep3$club))
