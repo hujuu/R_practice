cd86e0c533d2e54d40d56f54768bfbf0db40111b
hujuu <out.of.the.base.sfc@gmail.com>
2016-06-10 22:18:37 +0900

    10章突入

---
+++ r-every_10.1.R
@@ -0,0 +1,29 @@
+#フルーツの名前を持っているベクトルを作成
+fruit <- c("apple", "banana", "pomegranate")
+
+#フルーツベクトルの長さの分だけ、初期値としてNAをもつ変数を作る
+fruitLength <- rep(NA, length(fruit))
+
+#結果を表示する。全てNAとなる
+fruitLength
+
+#名前を与える
+names(fruitLength) <- fruit
+
+fruitLength
+
+for(a in fruit){
+  fruitLength[a] <- nchar(a)
+}
+
+fruitLength2 <- nchar(fruit)
+names(fruitLength2) <- fruit
+fruitLength2
+
+identical(fruitLength, fruitLength2)
+
+x <- 1
+while(x <= 5){
+  print(x)
+  x <- x + 1
+}
