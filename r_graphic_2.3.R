c44b369a7f7d953417c346e3c10edf2a4aeccdc1
hujuu <out.of.the.base.sfc@gmail.com>
2016-09-01 01:40:29 +0900

    bar plot

--- r_graphic_2.3.R
+++ r_graphic_2.3.R
@@ -16,3 +16,14 @@ qplot(mtcars$cyl)
 
 # binwidthを指定しろと言われるので、入れる
 qplot(mtcars$cyl, binwidth = 0.8)
+
+# cylを離散値として扱う
+qplot(factor(mtcars$cyl))
+
+# 値を示す棒グラフ。同じ意味だが最初のは現行バージョンでは動かない
+## qplot(Time, demand, data = BOD, geom = "bar", stat = "identity")
+ggplot(BOD, aes(x = Time, y = demand)) + geom_bar(stat = "identity")
+
+# 個数を示す棒グラフ
+qplot(factor(cyl), data = mtcars)
+ggplot(mtcars, aes(x = factor(cyl))) + geom_bar()
